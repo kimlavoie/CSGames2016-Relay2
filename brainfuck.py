7a2b2835cdce0bcc80084e6d6059a51bf0c9d1a5
Frédéric Bolduc <bolducfrederic@gmail.com>
2016-02-15 20:55:39 -0500

    HAVE FUN MATHIEU-SAN

--- brainfuck.py
+++ brainfuck.py
@@ -1,54 +1,80 @@
-##Information and TODO at the end
+import sys
 
-##30 000 cells (brainfuck array)
+# The brainfuck program string
+program = ""
+
+# 30 000 cells (brainfuck array)
 brainfuckArray = []
-for i in range(30000):
-    brainfuckArray.append(0)
-    
-##brainfuck pointer
-currentPtr = 0
 
-##Handle the current char of the input
-def HandleInput(charInput):
-    ##Increment current cell
-    if charInput == '+':
-        brainfuckArray[currentPtr] += 1
-    
-    ##Decrement current cell
-    elif charInput == '-':
-        brainfuckArray[currentPtr] -= 1
-    
-    ##Move Data pointer to next cell
-    elif charInput == '>':
-        currentPtr += 1
-        if currentPtr > 30000:
-            currentPtr = 30000
-    
-    ##Move Data pointer to previous cell
-    elif charInput == '<':
-        currentPtr -= 1
-        if currentPtr < 0:
-            currentPtr = 0
-    
-    ##Print
-    elif charInput == '.':
-        str(unichr(brainfuckArray[currentPtr])) ##should work by not tested
-        
-    elif charInput == ',':
-        
-        
-    elif charInput == '[':
-        
-        
-    elif charInput == ']':
-        
-        
-##Read every character of the input file
-##Use HandleInput with every character
+# Cursor in the brainfuck program (program)
+programCursor = 0
 
-## TODO
-## Read all character from file
-## Handle Character:     , [ ]
-## Make/Find a brainfuck file to test the program
+# Cursor in the brainfuck memory array (brainfuckArray)
+memCursor = 0
 
-##brainfuck doc: learnxinyminutes.com/docs/brainfuck/
+result = ""
+
+def main():
+    global program, brainfuckArray, programCursor, result
+
+    file = open(sys.argv[1])
+    
+    # The input program string
+    program = file.read()
+
+    for i in range(30000):
+        brainfuckArray.append(0)
+
+    while programCursor < len(program):
+        HandleInput()
+        programCursor += 1
+
+    print(result)
+    file.close()
+        
+# Handle the current char of the input
+def HandleInput():
+    global brainfuckArray, memCursor, programCursor, result
+
+    charInput = program[programCursor]
+
+    # Increment current cell
+    if (charInput == '+'):
+        brainfuckArray[memCursor] += 1
+    
+    # Decrement current cell
+    elif (charInput == '-'):
+        brainfuckArray[memCursor] -= 1
+    
+    # Move Data pointer to next cell
+    elif (charInput == '>'):
+        memCursor += 1
+        if memCursor > 30000:
+            memCursor = 30000
+    
+    # Move Data pointer to previous cell
+    elif (charInput == '<'):
+        memCursor -= 1
+        if memCursor < 0:
+            memCursor = 0
+
+    # Print
+    elif (charInput == '.'):
+        result += chr(brainfuckArray[memCursor])
+        
+    elif (charInput == ','):
+        brainfuckArray[memCursor] = ord(sys.stdin.read(1))
+        
+    elif (charInput == '['):
+        if (brainfuckArray[memCursor] == 0):
+            while (program[programCursor] != ']'):
+                programCursor += 1
+        
+    elif (charInput == ']'):
+        if (brainfuckArray[memCursor] != 0):
+            while (program[programCursor] != '['):
+                programCursor -= 1
+
+# Do not disturb this sacred piece of code
+if __name__ == "__main__":
+    main()
